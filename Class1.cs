f839385112ed9ab0adc16016f201c4f4215586ec
Vadde Nagamani <vadde.nagamani@ltts.com>
2021-05-06 11:19:06 +0530

    initializing repository

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld
+{
+    public class Class1
+    {
+        static void Main() { 
+        Console.WriteLine("Hello World!");
+        }
+    }
+}
